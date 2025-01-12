ebe64062156c99dd9611ed0359444bbd9404f357
cartermp <pcarter@fastmail.com>
2025-01-11 17:36:27 -0800

    link clicks

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,15 @@ export default function Home() {
     <div className="prose dark:prose-invert">
       {allPosts.map((post) => (
         <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
+          <Link 
+            href={post.slug}
+            className="no-underline"
+          >
+            <h2 className="text-gray-600 dark:text-gray-400
+                           hover:text-purple-700 dark:hover:text-purple-300
+                           transition-colors duration-200">
+              {post.title}
+            </h2>
           </Link>
           {post.description && <p>{post.description}</p>}
         </article>
