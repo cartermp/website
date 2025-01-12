734bfed48b9bd7ca0835714492ce7e0ca91cf0fd
cartermp <pcarter@fastmail.com>
2025-01-11 20:31:23 -0800

    post title colors

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -70,7 +70,7 @@ export default async function PostPage({ params }: PostProps) {
 
   return (
     <article className="prose dark:prose-invert max-w-none py-6">
-      <h1 className="mb-2">{post.title}</h1>
+      <h1 className="mb-2 text-purple-700 dark:text-purple-300">{post.title}</h1>
       {post.description && (
         <p className="text-xl mt-0 text-slate-700 dark:text-slate-200">
           {post.description}
