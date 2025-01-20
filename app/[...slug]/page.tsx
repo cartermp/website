13967b071593b595e0bfae0d9997e779af7688a9
cartermp <pcarter@fastmail.com>
2025-01-19 19:36:44 -0800

    about page

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -51,9 +51,8 @@ export default async function PagePage({ params }: PageProps) {
 
   return (
     <article className="py-6 prose dark:prose-invert">
-      <h1>{page.title}</h1>
+      <h1 className="mb-2 text-purple-700 dark:text-purple-300">{page.title}</h1>
       {page.description && <p className="text-xl">{page.description}</p>}
-      <hr />
       <Mdx code={page.body.code} />
     </article>
   )
