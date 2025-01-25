d188aa9028197c9190be371617b0ed855f9fce4e
cartermp <pcarter@fastmail.com>
2025-01-25 15:39:28 -0800

    add auth just for meeeeee

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+import type { User } from 'next-auth'
+
+export const authOptions = {
+    providers: [
+        Google({
+            clientId: process.env.GOOGLE_ID!,
+            clientSecret: process.env.GOOGLE_SECRET!,
+        }),
+    ],
+    callbacks: {
+        async signIn({ user }: { user: User }) {
+          const allowedEmail = process.env.ALLOWED_EMAIL?.trim();
+          const userEmail = user.email?.trim();
+          return allowedEmail === userEmail;
+        }
+    },
+}
+
+export const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
