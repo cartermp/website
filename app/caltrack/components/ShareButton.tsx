bdbc0d87d2f189a489c43ac66437df0f6ee389b8
cartermp <pcarter@fastmail.com>
2025-02-12 18:04:04 -0800

    escape

--- app/caltrack/components/ShareButton.tsx
+++ app/caltrack/components/ShareButton.tsx
@@ -37,7 +37,7 @@ export function ShareButton({ date }: { date: string }) {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
           </svg>
-          <span className="hidden sm:inline">Share Today's Report</span>
+          <span className="hidden sm:inline">Share Today&apos;s Report</span>
           <span className="sm:hidden">Share</span>
         </>
       )}
